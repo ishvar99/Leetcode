bc69ba6958bd6e69e9f037c06bb9521924673c90
Ishan Varshney <44304220+ishvar99@users.noreply.github.com>
2026-01-07 12:37:38 +0530

    Create 5_Longest_Palindromic_Substring.cpp

---
+++ Strings/5_Longest_Palindromic_Substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.length() <= 1) return s;
+
+        int start = 0, maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+
+            int currentMax = max(len1, len2);
+            
+            if (currentMax > maxLen) {
+                maxLen = currentMax;
+                start = i - (currentMax - 1) / 2;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+
+private:
+    int expandAroundCenter(const string& s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+};
