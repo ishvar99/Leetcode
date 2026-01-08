4446da642797bd53da0ac1ee6865e40fb4dc126d
Ishan Varshney <44304220+ishvar99@users.noreply.github.com>
2026-01-08 19:06:58 +0530

    Solved Leetcode 35

---
+++ Binary Search/35_Search_Insert_Position.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    }
+};
