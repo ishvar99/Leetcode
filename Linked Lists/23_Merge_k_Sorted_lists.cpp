5e8445024f8eab804a67cdbc62302fb11432cd71
Ishan Varshney <44304220+ishvar99@users.noreply.github.com>
2026-01-26 20:25:27 +0530

    Create 23_Merge_k_Sorted_lists.cpp

---
+++ Linked Lists/23_Merge_k_Sorted_lists.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    struct compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compare> minHeap;
+
+        for (auto list : lists) {
+            if (list) {
+                minHeap.push(list);
+            }
+        }
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while (!minHeap.empty()) {
+            ListNode* top = minHeap.top();
+            minHeap.pop();
+
+            tail->next = top;
+            tail = tail->next;
+            if (top->next) {
+                minHeap.push(top->next);
+            }
+        }
+
+        return dummy.next;
+    }
+};
